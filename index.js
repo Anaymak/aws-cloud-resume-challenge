c16b6c620f86ea6aef8c48b8087f2dd905256f7d
Anaymak <112899108+Anaymak@users.noreply.github.com>
2024-09-03 20:34:28 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Javascript Code
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("");
+    let data = await response.json();
+    counter.innerHTML = ' Views: ${data}'; 
+}
+
+updateCounter();
