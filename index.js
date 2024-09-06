f664593b63b57e41563b5190dcec8f86c5c42327
Anaymak <112899108+Anaymak@users.noreply.github.com>
2024-09-06 14:17:44 +0100

    updated views function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Javascript Code
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("");
+    let response = await fetch("https://wr7xsrh23w5a5ucf6tjxtcnhbi0iitvv.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ' Views: ${data}'; 
 }
